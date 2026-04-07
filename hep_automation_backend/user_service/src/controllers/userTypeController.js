aafcb270b584fb446b0054ccc9c00260c0b9d340
Madhav Shri <vallabhaimadhavan4@gmail.com>
2026-04-07 11:21:16 +0530

    minor change

--- hep_automation_backend/user_service/src/controllers/userTypeController.js
+++ hep_automation_backend/user_service/src/controllers/userTypeController.js
@@ -1,36 +1,26 @@
 const UserType = require("../models/userTypeSchema");
-const generateCaptcha = require("../utils/captchaGenerator");
-const { saveCaptcha } = require("../utils/captchaStore");
-const crypto = require("crypto");
+const captchaService = require("../services/captchaService");
 
 exports.getUserTypes = async (req, res) => {
-
   try {
-
     const userTypes = await UserType.getAllUserTypes();
 
-    const captcha = generateCaptcha();
-
-    const captchaToken = crypto.randomUUID();
-
-    saveCaptcha(captchaToken, captcha.text);
+    const captcha = await captchaService.createCaptcha();
 
     res.status(200).json({
       success: true,
       userTypes: userTypes,
-      captchaSvg: captcha.svg,
-      captchaToken: captchaToken
+      captchaSvg: captcha.captchaSvg,
+      captchaToken: captcha.captchaToken,
+      expiresIn: captcha.expiresIn
     });
 
   } catch (error) {
-
     console.error("Error fetching user types:", error);
 
     res.status(500).json({
       success: false,
       message: "Internal server error"
     });
-
   }
-
 };
